c264c25cf22572421e7926d433674a08e2b0d948
Adolfo <44960428+adolfcg@users.noreply.github.com>
2023-02-28 17:17:26 -0600

    Update resource_server.go

--- resource_server.go
+++ resource_server.go
@@ -2,59 +2,64 @@
 package main
 
 import (
-        "io"
-        "log"
-        "net/http"
-        "github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"io"
+	"log"
+	"net/http"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 )
 
 func newOrderResource() *schema.Resource {
-        return &schema.Resource{
-                Create: newOrderCreate,
-                Read:   newOrderRead,
-                Update: newOrderUpdate,
-                Delete: newOrderDelete,
+	return &schema.Resource{
+		Create: newOrderCreate,
+		Read:   newOrderRead,
+		Update: newOrderUpdate,
+		Delete: newOrderDelete,
 
-                Schema: map[string]*schema.Schema{
-                        "style": &schema.Schema{
-                                Type:     schema.TypeString,
-                                Required: true,
-                        },
-                        "size": &schema.Schema{
-                                Type:     schema.TypeString,
-                                Required: true,
-                        },
-                },
-        }
+		Schema: map[string]*schema.Schema{
+			"style": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"size": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"place": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+		},
+	}
 }
 
 func newOrderCreate(d *schema.ResourceData, m interface{}) error {
-        resp, err := http.Get("https://www.uuidtools.com/api/generate/v1/count/1")
-        if err != nil {
-                log.Fatal(err)
-        }
-        defer resp.Body.Close()
-        if resp.StatusCode == http.StatusOK {
-                bodyBytes, err := io.ReadAll(resp.Body)
-                if err != nil {
-                        log.Fatal(err)
-                }
-                bodyString := string(bodyBytes)
-                d.SetId(bodyString)
-        }
+	resp, err := http.Get("https://www.uuidtools.com/api/generate/v1/count/1")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode == http.StatusOK {
+		bodyBytes, err := io.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		bodyString := string(bodyBytes)
+		d.SetId(bodyString)
+	}
 
-        return newOrderRead(d, m)
+	return newOrderRead(d, m)
 }
 
 func newOrderRead(d *schema.ResourceData, m interface{}) error {
-        return nil
+	return nil
 }
 
 func newOrderUpdate(d *schema.ResourceData, m interface{}) error {
-        return newOrderRead(d, m)
+	return newOrderRead(d, m)
 }
 
 func newOrderDelete(d *schema.ResourceData, m interface{}) error {
-        d.SetId("")
-        return nil
+	d.SetId("")
+	return nil
 }
